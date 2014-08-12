9080ecf08da36ac0286545842e96adc2ce06ba7e
danshapero <shapero.daniel@gmail.com>
2014-08-12 11:27:11 -0700

    Changed the directory structure

--- linalg/linalg.h
+++
@@ -1,9 +0,0 @@
-#include <jit/jit.h>
-#include <math.h>
-
-typedef double (*dot_product) (int n, double x[n], double y[n]);
-
-double native_dot_product(int n, double x[n], double y[n]);
-
-jit_function_t build_dot_product(jit_context_t context);
-jit_function_t build_unrolled_dot_product(jit_context_t context, unsigned int r);
